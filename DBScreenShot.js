5aa5fff5e177f761a53362d91a5b8589778d71be
TowhidAust <007towhid2016@gmail.com>
2021-01-30 16:50:34 +0600

    db schema

---
+++ DBScreenShot.js
@@ -0,0 +1,43 @@
+// sample database structure
+let db = {
+    "users" : {
+      "-MSENrLz3I7hVBJcLVD5" : {
+        "publicInfo" : {
+          "address" : "new address",
+          "email" : "new@gmail.com",
+          "name" : "new name",
+          "password" : "1234",
+          "token" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im5ld0BnbWFpbC5jb20iLCJpYXQiOjE2MTIwMDIwMjMsImV4cCI6MTYxMjAwNTYyM30.Qk2E8sxVvsZlO8nI84bAbGEWDSsZ03z7d6rv7aXD4wM"
+        }
+      },
+      "-MSHItzot7jo8jXlTJWg" : {
+        "publicInfo" : {
+          "address" : "danmondi",
+          "email" : "gg@gmail.com",
+          "name" : "md. towhidul",
+          "password" : "12345",
+          "token" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdnQGdtYWlsLmNvbSIsImlhdCI6MTYxMjAwMjY5MywiZXhwIjoxNjEyMDA2MjkzfQ.jXB3F0CVzzeUdsQwNJgim_42sBbOjAFJuOptMalb5f8"
+        }
+      }
+    },
+
+
+
+    "menu" : {
+        "-MSHr27rjRQHlwKxmMUs": {
+        "restaurantid" : 1,
+        "restaurantname" : "kacchivai",
+          "drinks" : {
+            "coke" : "20",
+            "pepsi" : "20",
+            "water" : "20"
+          },
+          "junk-food" : {
+            "fries" : "20",
+            "hamburger" : "40",
+            "pizza" : "20"
+          },
+          
+        }
+      },
+  }
